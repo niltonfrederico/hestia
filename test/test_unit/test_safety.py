29893c91e904ab97cdfadd552df2412cc0a8ddf3
Nilton Teixeira <niltonfrederico@pm.me>
2021-05-08 16:01:19 -0300

    Added test for safety

--- test/test_unit/test_safety.py
+++ test/test_unit/test_safety.py
@@ -1,6 +1,16 @@
-def test_app_safety():
-    pass
+from subprocess import Popen, PIPE
 
 
-def test_app_safety_garantee_that_can_catch_unsafety():
-    pass
+def test_app_requirements_safety():
+    with Popen(
+        ["safety", "check", "-r", "requirements/development.txt"],
+        stdin=PIPE,
+        stdout=PIPE,
+        stderr=PIPE,
+    ) as p:
+        output, _ = p.communicate()
+        rc_development = (
+            p.returncode
+        )  # If 0 has no vulnerable packages, if 255, it has.
+
+        assert rc_development != 255, f"\n==== Development:\n{output.decode()}"
